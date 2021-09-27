ecd5795bd21b4aac1379a402f13e43da2354b7f3
SreckoStojic <stojic.srecko@gmail.com>
2021-09-27 16:37:11 +0200

    Added border alert color, error image and input element fixed.

--- index.js
+++ index.js
@@ -9,55 +9,71 @@ form.addEventListener("submit", function(e){
     e.preventDefault();
     const firstName = document.querySelector(".js-error-firstName");
     const firstNameImg = document.querySelector(".js-error-img-firstName");
+    const firstNameDiv = document.querySelector(".js-input-firstName-div");
 
     const lastName = document.querySelector(".js-error-lastName");
     const lastNameImg = document.querySelector(".js-error-img-lastName");
+    const lastNameDiv = document.querySelector(".js-input-lastName-div");
 
     const email = document.querySelector(".js-error-email");
     const emailImg = document.querySelector(".js-error-img-email");
+    const emailDiv = document.querySelector(".js-input-email-div");
     
 
     const password = document.querySelector(".js-error-password");
     const passwordImg = document.querySelector(".js-error-img-password");
+    const passwordDiv = document.querySelector(".js-input-password-div");
     
-    
+    const cssErrorMsgVisibleClass = "css-error-msg-visible";
+    const cssErrorImgVisibleClass = "css-error-img-visible";
+    const cssInputErrorClass = "css-input-error";
+
     if(e.target.elements.firstName.value === "") {
-        firstName.classList.add("css-error-msg-visible");
-        firstNameImg.classList.add("css-error-img-visible");
+        firstName.classList.add(cssErrorMsgVisibleClass);
+        firstNameImg.classList.add(cssErrorImgVisibleClass);
+        firstNameDiv.classList.add(cssInputErrorClass);
     } else {
-        firstName.classList.remove("css-error-msg-visible");
-        firstNameImg.classList.remove("css-error-img-visible");
+        firstName.classList.remove(cssErrorMsgVisibleClass);
+        firstNameImg.classList.remove(cssErrorImgVisibleClass);
+        firstNameDiv.classList.remove(cssInputErrorClass);
     }
     
     if(e.target.elements.lastName.value === "") {
-        lastName.classList.add("css-error-msg-visible");
-        lastNameImg.classList.add("css-error-img-visible");
+        lastName.classList.add(cssErrorMsgVisibleClass);
+        lastNameImg.classList.add(cssErrorImgVisibleClass);
+        lastNameDiv.classList.add(cssInputErrorClass);
     } else {
-        lastName.classList.remove("css-error-msg-visible");
-        lastNameImg.classList.remove("css-error-img-visible");
+        lastName.classList.remove(cssErrorMsgVisibleClass);
+        lastNameImg.classList.remove(cssErrorImgVisibleClass);
+        lastNameDiv.classList.remove(cssInputErrorClass);
     }
 
 
     if(validateEmail(e.target.elements.email.value)) {
-        email.classList.remove("css-error-msg-visible");
-        emailImg.classList.remove("css-error-img-visible");
+        email.classList.remove(cssErrorMsgVisibleClass);
+        emailImg.classList.remove(cssErrorImgVisibleClass);
+        emailDiv.classList.remove(cssInputErrorClass);
     } else {
-        email.classList.add("css-error-msg-visible");
-        emailImg.classList.add("css-error-img-visible");
+        email.classList.add(cssErrorMsgVisibleClass);
+        emailImg.classList.add(cssErrorImgVisibleClass);
+        emailDiv.classList.add(cssInputErrorClass);
     }
 
    
     if(e.target.elements.password.value === "") {
-        password.classList.add("css-error-msg-visible");
-        passwordImg.classList.add("css-error-img-visible");
+        password.classList.add(cssErrorMsgVisibleClass);
+        passwordImg.classList.add(cssErrorImgVisibleClass);
+        passwordDiv.classList.add(cssInputErrorClass);
     } else {
-        password.classList.remove("css-error-msg-visible");
-        passwordImg.classList.remove("css-error-img-visible");
+        password.classList.remove(cssErrorMsgVisibleClass);
+        passwordImg.classList.remove(cssErrorImgVisibleClass);
+        passwordDiv.classList.remove(cssInputErrorClass);
     }
     
     const listOfErrors = document.querySelectorAll(".css-error-msg-visible");
     console.log(listOfErrors);
     if (listOfErrors.length === 0) {
         alert("Successfully claimed Free trial!");
+        form.reset();
     }
 });
